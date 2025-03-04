7da3820220d77480a52ac62ef78a4ffaf9c2c44e
logith aadithya <logithaadithya@logiths-MacBook-Air.local>
2025-03-04 18:37:15 +0530

    updated img

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
     const nav = document.querySelector('.nav-links');
+    const mobileSearch = document.querySelector('.mobile-search');
     nav.classList.toggle('active');
+    mobileSearch.style.display = nav.classList.contains('active') ? 'flex' : 'none';
 }
